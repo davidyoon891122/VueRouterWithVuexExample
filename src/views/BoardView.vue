34f284ef8c003e03924ece9e344725ba4f60239f
davidyoon <qqpo12@naver.com>
2022-08-24 14:09:06 +0900

    Add a BoardView.vue
    * Added a BoardView
    * Added a Router-link

---
+++ src/views/BoardView.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="board">
+        <p>이곳은 {{boardName}} 게시판 입니다.</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "BoardView",
+    date() {
+        return {
+            boardName: ""
+        }
+    },
+    created() {
+        this.boardName = this.$route.params.id;
+    }
+}
+</script>
